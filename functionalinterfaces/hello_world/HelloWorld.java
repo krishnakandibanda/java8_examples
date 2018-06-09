65f1f1659c2ec145240660c86f030afeedd54d56
krishnakandibanda <krishna.kandibanda@gmail.com>
2018-06-09 18:01:04 +0530

    Add Welcome message

--- functionalinterfaces/hello_world/HelloWorld.java
+++ functionalinterfaces/hello_world/HelloWorld.java
@@ -3,7 +3,10 @@ package hello_world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Java8 examples");
+
+		System.out.println("Welcome to Krishna Java8");
 
 	}
 
